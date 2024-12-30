d9cdaff9e8958970fea83086468ddf47878dfe97
Csquare21997 <biscuitxc@gmail.com>
2024-12-29 22:19:01 -0500

    3

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,64 @@
+import axios from 'axios'
 import React from 'react'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+state = {
+  todos: [],
+  error: '',
+  todoNameInput: '',
+}
+onTodoNameInputChange = evt => {
+  const {value} = evt.target
+  this.setState ({...this.state, todoNameInput:value})
+}
+postNewTodo = () => {
+  axios.post(URL, {name:this.state.todoNameInput})
+  .then (res=> {
+    this.fetchAllTodos()
+    this.setState ({...this.state, todoNameInput: ''})
+  })
+  .catch (err =>{
+    this.setState({...this.state, error:err.response.data.message })
+  })
+}
+onTodoFormSubmit = evt => {
+  evt.preventDefault ()
+  this.postNewTodo ()
+}
+fetchAllTodos = () => {
+  axios.get (URL)
+  .then(res=> {
+    this.setState({...this.state, todos: res.data.data })
+  })
+  .catch(err => {
+    this.setState({...this.state, error:err.response.data.message })
+})
+}
+componentDidMount () {
+  this.fetchAllTodos()
+   
+}
+
   render() {
-    return null
+    return (
+      <div>
+        <div id="error">Error:{this.state.error}</div>
+        <h2>Todos:</h2>
+        {
+          this.state.todos.map (td => {
+            return <div key={td.id}>{td.name}</div>
+          })
+        }
+        <form id ="todoForm" onSubmit={this.onTodoFormSubmit}>
+        <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange} type ="text" placeholder="Type todo"></input>
+        <input type='submit'></input>
+        <button>Clear Completed</button>
+
+        </form>
+
+      </div>
+    )
   }
 }
